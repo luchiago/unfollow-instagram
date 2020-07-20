b7b7e6b56236f21116f8cecb9655f0e54d6a6f62
Lucas Hiago <lucashiago63@gmail.com>
2020-07-20 08:27:39 -0300

    change the keys for user ig credentials

--- settings.py
+++ settings.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-USERNAME = os.getenv("USERNAME")
-PASSWORD = os.getenv("PASSWORD")
+USERNAME = os.getenv("IG_USERNAME")
+PASSWORD = os.getenv("IG_PASSWORD")
+NUMBER = os.getenv("IG_NUMBER")
 TWO_FACTOR = os.getenv("TWO_FACTOR")
-USERNAMES_FILE = os.getenv("USERNAMES_FILE")
+USERNAMES_FILENAME = os.getenv("USERNAMES_FILENAME")
