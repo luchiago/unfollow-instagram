2459e412d48b3e2dc79d17672b849ec3948026dc
Lucas Hiago <lucashiago63@gmail.com>
2020-07-20 08:28:11 -0300

    add login function

---
+++ login.py
@@ -0,0 +1,33 @@
+from time import sleep
+
+from selenium.webdriver.common.keys import Keys
+
+from constants import BASE_URL, BROWSER
+from settings import PASSWORD, TWO_FACTOR, USERNAME
+
+
+def login():
+    BROWSER.get(f"{BASE_URL}/accounts/login")
+    sleep(5)
+    _ = find_input_and_send_keys("username", USERNAME)
+    passwordInput = find_input_and_send_keys("password", PASSWORD)
+    passwordInput.send_keys(Keys.ENTER)
+    if TWO_FACTOR:
+        """
+        If 2FA is enabled, the user 
+        will have time to put the code
+        """
+        sleep(30)
+        find_not_now_button("/html/body/div[1]/section/main/div/div/div/div/button")
+    # Notifications
+    find_not_now_button("/html/body/div[4]/div/div/div/div[3]/button[2]")
+
+
+def find_input_and_send_keys(name, key):
+    element = BROWSER.find_element_by_name(name)
+    element.send_keys(key)
+    return element
+
+
+def find_not_now_button(xpath):
+    BROWSER.find_element_by_xpath(xpath).click()
